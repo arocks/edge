d00e5ad27a22a58883154ab54f3518afde4ecc6c
Arun Ravindran <arunvr@gmail.com>
2015-01-31 16:15:32 +0530

    Added tests for profile

--- src/profiles/tests.py
+++ src/profiles/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve, reverse
+from django.contrib.auth import get_user_model
 from .views import SignInAndSignUp
 
 
@@ -19,3 +20,12 @@ class PageOpenTestCase(TestCase):
         url = reverse('about')
         r = self.client.get(url)
         self.assertEqual(r.status_code, 200)
+
+
+User = get_user_model()
+
+
+class ProfileTestCase(TestCase):
+    def test_profiles_created(self):
+        u = User.objects.create_user(username="dummy")
+        self.assertIsNotNone(u.profile)
