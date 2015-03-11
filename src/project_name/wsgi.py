195d260a8ee56f7ecd0aac7594743f59ea1a1c34
Arun Ravindran <arunvr@gmail.com>
2015-03-12 03:58:43 +0530

    Added Werkzeug debugger support in debug mode

--- src/project_name/wsgi.py
+++ src/project_name/wsgi.py
@@ -6,9 +6,24 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
-
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ project_name }}.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+# Wrap werkzeug debugger if DEBUG is on
+from django.conf import settings
+if settings.DEBUG:
+    try:
+        import django.views.debug
+        import six
+        from werkzeug.debug import DebuggedApplication
+
+        def null_technical_500_response(request, exc_type, exc_value, tb):
+            six.reraise(exc_type, exc_value, tb)
+
+        django.views.debug.technical_500_response = null_technical_500_response
+        application = DebuggedApplication(application, evalex=True)
+    except ImportError:
+        pass
