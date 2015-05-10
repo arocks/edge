a15b70924b9ec966c658f014078c336f1a8d2fe6
Arun Ravindran <arunvr@gmail.com>
2015-05-10 18:21:05 +0530

    Changed to UUIDField for profile.slug

--- src/profiles/urls.py
+++ src/profiles/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^me$', views.ShowProfile.as_view(), name='show_self'),
     url(r'^me/edit$', views.EditProfile.as_view(), name='edit_self'),
-    url(r'^(?P<slug>\w+)$', views.ShowProfile.as_view(),
+    url(r'^(?P<slug>[\w\-]+)$', views.ShowProfile.as_view(),
         name='show'),
 ]
