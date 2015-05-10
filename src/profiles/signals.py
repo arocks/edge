b61099a80c5e6e1f6b31fda5032330c60805622a
Arun Ravindran <arunvr@gmail.com>
2015-05-10 15:29:03 +0530

    Add logging for new user creation and more verbosity

--- src/profiles/signals.py
+++ src/profiles/signals.py
@@ -1,7 +1,10 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.conf import settings
+import logging
 from . import models
+
+logger = logging.getLogger("project")
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
@@ -11,3 +14,4 @@ def create_profile_handler(sender, instance, created, **kwargs):
     # Create the profile object, only if it is newly created
     profile = models.Profile(user=instance)
     profile.save()
+    logger.info('New user profile for {} created'.format(instance))
--- src/project_name/settings/development.py
+++ src/project_name/settings/development.py
@@ -31,7 +31,7 @@ LOGGING = {
     'disable_existing_loggers': False,
     'formatters': {
         'verbose': {
-            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'format': "[%(asctime)s] %(levelname)s [%(pathname)s:%(lineno)s] %(message)s",
             'datefmt': "%d/%b/%Y %H:%M:%S"
         },
         'simple': {
