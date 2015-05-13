c3a1f3f89f63d40e3e8403e17bea03bfccea690c
Arun Ravindran <arunvr@gmail.com>
2015-05-13 14:02:37 +0530

    Squashed profiles migrations

--- src/profiles/migrations/0001_initial.py
+++ src/profiles/migrations/0001_initial.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 from django.conf import settings
+import uuid
 
 
 class Migration(migrations.Migration):
@@ -16,7 +17,7 @@ class Migration(migrations.Migration):
             name='Profile',
             fields=[
                 ('user', models.OneToOneField(serialize=False, primary_key=True, to=settings.AUTH_USER_MODEL)),
-                ('slug', models.SlugField(unique=True, max_length=32, editable=False, blank=True)),
+                ('slug', models.UUIDField(default=uuid.uuid4, blank=True, editable=False)),
                 ('picture', models.ImageField(verbose_name='Profile picture', upload_to='profile_pics/%Y-%m-%d/', blank=True, null=True)),
                 ('bio', models.CharField(verbose_name='Short Bio', max_length=200, blank=True, null=True)),
                 ('email_verified', models.BooleanField(default=False, verbose_name='Email verified')),
