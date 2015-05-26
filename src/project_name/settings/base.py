c93072fbd30fbaafbc7bc47ffabcbdd94a6e933a
Arun Ravindran <arunvr@gmail.com>
2015-05-26 20:02:37 +0530

    Reordering installed apps due to Auth issue in migration.

--- src/project_name/settings/base.py
+++ src/project_name/settings/base.py
@@ -63,20 +63,21 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'authtools',
+    'django.contrib.auth',
     'django_admin_bootstrapped',
+    'django.contrib.admin',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'authtools',
     'crispy_forms',
     'easy_thumbnails',
 
     'profiles',
     'accounts',
 
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
 )
 
 MIDDLEWARE_CLASSES = (
