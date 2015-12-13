2f6315ebcc17e1ae00104f67b9094d3c4412bc93
Arun Ravindran <arunvr@gmail.com>
2015-12-13 11:18:46 +0530

    Added blank line

--- src/project_name/settings/development.py
+++ src/project_name/settings/development.py
@@ -5,6 +5,7 @@ import logging.config
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 TEMPLATES[0]['OPTIONS'].update({'debug': True})
+
 # Turn off debug while imported by Celery with a workaround
 # See http://stackoverflow.com/a/4806384
 if "celery" in sys.argv[0]:
